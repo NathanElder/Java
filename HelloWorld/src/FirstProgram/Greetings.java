5b233d8cbc606775a5934852020944edea8721f9
Nathan E <e.nathan@yahoo.com>
2015-08-15 03:39:40 -0400

    Hello World with Java

---
+++ HelloWorld/src/FirstProgram/Greetings.java
@@ -0,0 +1,9 @@
+package FirstProgram;
+
+
+class Salutation {
+    public static void main (String[] arguments) {
+        String greeting =  "Hello World!";
+        System.out.println(greeting);
+    }
+}
